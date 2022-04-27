8253cd05d4129aa8ef7bf284ffa286f234ae4432
ignasprak <ignasprak@gmail.com>
2022-04-27 18:24:30 +0100

    step3
    got the text to work

--- java/src/ie/tudublin/NematodeVisualiser.java
+++ java/src/ie/tudublin/NematodeVisualiser.java
@@ -1,16 +1,29 @@
 package ie.tudublin;
 
+import java.util.ArrayList;
 import processing.core.PApplet;
 import processing.data.Table;
 import processing.data.TableRow;
 
 public class NematodeVisualiser extends PApplet
 {
+	ArrayList<Nematode> nematode = new ArrayList<Nematode>();
 
-	public void keyPressed()
-	{
-		if (keyCode == LEFT)
-		{
+	int index = 0;
+
+	public void keyPressed() {
+		if (keyCode == LEFT) {
+			index = index - 1;
+			if (index < 0) {
+				index = 12;
+			}
+		}
+
+		if (keyCode == RIGHT) {
+			index = index + 1;
+			if (index > 12) {
+				index = 0;
+			}
 		}
 	}
 
@@ -23,24 +36,35 @@ public class NematodeVisualiser extends PApplet
 	{
 		colorMode(HSB);
 		background(0);
-		smooth();
-
 		loadNematodes();
+		smooth();
 	}
+
+	int choice = 0;
+	float radius = 75.0f;
 
 	public void loadNematodes()
 	{
-		Table table = loadTable("nematodes.csv", "header");
+	Table table = loadTable("nematodes.csv", "header");
 
-		for(TableRow r:table.rows())
-		{
-			Nematode n = new Nematode(r);
-			System.out.println(n);
-		}
+	 	for(TableRow r:table.rows())
+	 	{
+	 		Nematode n = new Nematode(r);
+	 		nematode.add(n);
+	 		System.out.println(n);
+	 	}
 	}
 
-	public void draw()
-	{
+	public void draw() {
+		int n = nematode.size();
+		int currentIndex = index % n;
+		//start of draw
+		clear();
 
+		//text
+		textSize(80);
+		textAlign(CENTER);
+		String Name = nematode.get(currentIndex).getName();
+		text(Name, 400, 80);
 	}
 }
